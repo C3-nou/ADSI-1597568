16bf7e116f2f6f9f1d01fd08e88bc27174eb5c0f
Cristian Camilo Cadavid Escarraga <cristiancadavid45@gmail.com>
2018-04-20 10:40:56 -0500

    First Work
    My first lines in PHP

---
+++ Analysis/5. PHP/MiPrimerProyecto/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require 'manners.php';
+
+use MyFirstProject\first;
+
+$manners = new first();
+$manners->thing();
+$manners->change();
---
+++ Analysis/5. PHP/MiPrimerProyecto/manners.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MyFirstProject;
+
+class first {
+
+  public function thing() : void {
+    echo 'Hello, my name is Cristian';
+    echo '<br>';
+  }
+  public function change() : void {
+    echo 'This is my second message in PHP through object-oriented programming.';
+  }
+}
+
+
